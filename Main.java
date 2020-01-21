34bebbe3841bda596cdaa55a56f5eb55911a0a55
Filip Hedlund <fhedl@kth.se>
2020-01-21 17:04:10 +0100

    Completed code skeleton, resolves #4
    Added parameters and LIC functions to CMV class.
    Added global variables to Main class.
    Removed Parameters class.

--- Main.java
+++ Main.java
@@ -1,8 +1,26 @@
+import java.awt.Point;
+
 public class Main{
-	
+
+    enum LogicalOperators {
+  	   NOTUSED,
+  	   ORR,
+ 	     ANDD
+    }
+
+    int NUMPOINTS;
+    Point[] POINTS;
+    Parameters PARAMETERS;
+    LogicalOperators[][] LCM = new LogicalOperators[15][15];
+    Boolean[] PUV = new Boolean[15];
+    String LAUNCH;
+    Boolean[] CMV = new Boolean[15];
+    Boolean[][] PUM = new Boolean[15][15];
+    Boolean[] FUV = new Boolean[15];
+
     public static void main(String[] args){
-	
-	
+
+
     }
 
 }
