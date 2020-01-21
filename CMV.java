9a64ddbd686f1fdaf0463d7f940c91d934f277f2
Filip Hedlund <fhedl@kth.se>
2020-01-21 15:57:21 +0100

    Add skeleton classes and functions
    Added CMV class with empty fillCMV function.
    Added Parameters class as struct for LIC paramaters.

---
+++ CMV.java
@@ -0,0 +1,7 @@
+public class CMV{
+
+    public void fillCMV(){
+
+    }
+    
+}
---
+++ Parameters.java
@@ -0,0 +1,11 @@
+public class Paramaters{
+
+    //add data parameters		
+
+    public Paramaters(){
+
+    }
+
+    //add get-functions for parameters
+
+}
