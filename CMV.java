e662bd4a05235dab6b15eb5965b4aa77f447b71f
Julien <julien.vignoud@epfl.ch>
2020-01-24 07:35:31 +0100

    Add function DECIDE #22
    The function DECIDE is the main method of the class CMV.
    It returns the CMV by calling each LIC.

--- CMV.java
+++ CMV.java
@@ -15,8 +15,26 @@ public class CMV{
 
     }
 
-    public void fillCMV(Boolean[] CMV){
-
+    //Returns a new array implementing the cmv
+    //where each entry is true iff the relevant LIC is true
+    public boolean[] DECIDE(){
+      boolean [] cmv = new boolean[15];
+      cmv[0] = LIC0();
+      cmv[1] = LIC1();
+      cmv[2] = LIC2();
+      cmv[3] = LIC3();
+      cmv[4] = LIC4();
+      cmv[5] = LIC5();
+      cmv[6] = LIC6();
+      cmv[7] = LIC7();
+      cmv[8] = LIC8();
+      cmv[9] = LIC9();
+      cmv[10] = LIC10();
+      cmv[11] = LIC11();
+      cmv[12] = LIC12();
+      cmv[13] = LIC13();
+      cmv[14] = LIC14();
+      return cmv;
     }
 
     private boolean LIC0(){
