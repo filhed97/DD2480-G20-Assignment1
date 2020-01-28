a195aaeb2259ff9793a26f74f942597d6d79832d
filhed97 <filhed97@gmail.com>
2020-01-28 15:49:43 +0100

    Added tests for LIC 2,6,10,14. #37 #33 #29 #24
    Added test for LIC 2
    Added test for LIC 6
    Added test for LIC 10
    Added test for LIC 14
    Big fixes in Tests.java

--- Tests.java
+++ Tests.java
@@ -93,6 +93,7 @@ public class Tests {
       for (int i=0; i<15; i++) {
         assertThat(effective[i], equalTo(expected[i]));
       }
+	}
 
 
     //Tests that the FUV is filled accordingly to the PUM and the PUV:
@@ -154,7 +155,7 @@ public class Tests {
       Point b = new Point(1,1);
       Point c = new Point(1,2);
 	  Point d = new Point(0,1);
-	  Point e = new Point(1,0)
+	  Point e = new Point(1,0);
 	
 	  //Test 1, check angle < PI-EPSILON.
       Point[] data1 = {a, b, c}; //abc is a PI/2 rad angle
@@ -163,12 +164,13 @@ public class Tests {
       assertThat(cmv1.DECIDE()[2], equalTo(true));
 
 	  //Test 2, check angle > PI+EPSILON.
-      Point[] data2 = {a, b, f}; //abf is a 3PI/2 rad angle
-      CMV cmv2 = new CMV(5, data2, param);
+      Point[] data2 = {a, b, e}; //abe is a 3PI/2 rad angle
+      CMV cmv2 = new CMV(3, data2, param);
       //Should be success since 3PI/2 is larger than 5PI/4
       assertThat(cmv2.DECIDE()[2], equalTo(true));
 
 	  //Test 3, check if angle lays between PI-EPSILON and PI+EPSILON
+	  //d.setLocation(0.00,1.100);
       Point[] data3 = {a, b, d}; //abd is a PI rad angle
       CMV cmv3 = new CMV(3, data3, param);
       //Should be false since PI lies in PI +/- EPSILON
@@ -182,10 +184,129 @@ public class Tests {
 
 	  //Test 5, edge case where EPSILON = 0.
 	  param.EPSILON = 0;
-	  Point[] data1 = {a, b, d}; //abc is a PI rad angle
-      CMV cmv5 = new CMV(3, data1, param);
+	  Point[] data5 = {a, b, d}; //abc is a PI rad angle
+      CMV cmv5 = new CMV(3, data5, param);
       //Should be false, since angle should be strictly smaller or larger than PI.
-      assertThat(cmv1.DECIDE()[2], equalTo(false));
+      assertThat(cmv5.DECIDE()[2], equalTo(false));
     }
+	
+	//Tests true iff some point between i and i+NPTS has distance greater 
+	//than DIST between itself and the line created by the points i and i+NPTS
+	//If points i and i+NPTS coincide, distance is calculated from their joint position.
+	//If NUMPOINTS < 3, value should be false.
+	//Edge cases: 
+	//DIST = 0,
+	//Points i and i+NPTS are identical.
+	@Test
+    public void LIC6(){
+		
+		param.NPTS = 3;
+		param.DIST = 2;
+		
+		Point a = new Point(0,0);
+		Point b = new Point(1,3);
+		Point c = new Point(2,1);
+		Point d = new Point(3,0);
+		
+		//Test 1, check case where distance from middle point to line is 1, but dist is 2.
+		Point[] data1 = {a, c, d}; //In a coordinate system x/y, the line a-d follows the x-axis from 0 to 3.
+		CMV cmv1 = new CMV(3, data1, param);
+		//Should be false, since dist is 2, and the distance from c to the x-axis is 1.
+		assertThat(cmv1.DECIDE()[6], equalTo(false));
+		
+		//Test 2, check case where distance from middle point to line is 3, and dist is 2.
+		Point[] data2 = {a, b, d}; //In a coordinate system x/y, the line a-d follows the x-axis from 0 to 3.
+		CMV cmv2 = new CMV(3, data2, param);
+		//Should be true, since dist is 2, and the distance from b to the x-axis is 3.
+		assertThat(cmv2.DECIDE()[6], equalTo(true));
+		
+		//Test 3, check case where edge points coincide. 
+		Point[] data3 = {a, d, a};
+		CMV cmv3 = new CMV(3, data3, param);
+		//Should be true, since dist is 2, and the distance from a to d is 3.
+		assertThat(cmv3.DECIDE()[6], equalTo(true));
+		
+		//Test 4, check case where DIST = 0, i.e. every input should be true.
+		param.DIST = 0;
+		Point[] data4 = {a, a, d};
+		CMV cmv4 = new CMV(3, data4, param);
+		//Should be true, since points coinciding with the line are at least 0 distance away from the line.
+		assertThat(cmv4.DECIDE()[6], equalTo(true));
+		
+		//Test 5, not enough points.
+		Point[] data5 = {a, d};
+		CMV cmv5 = new CMV(2, data4, param);
+		//Should be false, not enough data.
+		assertThat(cmv5.DECIDE()[6], equalTo(false));
+		
+	}
+	
+	
+	//Tests true iff area created by points {i, i+EPTS+1, i+EPTS+FPTS+2} is larger than AREA1.
+	//NUMPOINTS >= 5.
+	@Test
+    public void LIC10(){
+		param.AREA1 = 1;
+		param.EPTS = 1;
+		param.FPTS = 1;
+		
+		Point a = new Point(0,0);
+		Point b = new Point(1,0);
+		Point c = new Point(0,1);
+		Point filler = new Point(0,0);
+		
+		//Test 1, check case where area is smaller than AREA1.
+		Point[] data1 = {a, filler, b, filler, c}; //creates a triangle with area 0.5
+		CMV cmv1 = new CMV(5, data1, param);
+		//Should be false, 0.5<1.
+		assertThat(cmv1.DECIDE()[10], equalTo(false));
+		
+		//Test 2, check case where area is smaller than AREA1.
+		param.AREA1 = 0.1;
+		CMV cmv2 = new CMV(5, data1, param); //triangle abc with area 0.5
+		//Should be true, 0.5>0.1.
+		assertThat(cmv2.DECIDE()[10], equalTo(true));
+		
+		//Test 3, not enough data.
+		Point[] data2 = {a, b, filler, c};
+		CMV cmv3 = new CMV(4, data2, param);
+		//Should be false, must have at least 5 data points.
+		assertThat(cmv3.DECIDE()[10], equalTo(false));
+	}
+	
+	
+	@Test
+	public void LIC14(){
+		param.AREA1 = 0.5;
+		param.AREA2 = 1;
+		param.EPTS = 1;
+		param.FPTS = 1;
+		
+		Point a = new Point(0,0);
+		Point b = new Point(1,0);
+		Point c = new Point(0,1);
+		Point d = new Point(2,0);
+		Point e = new Point(0,2);
+		Point filler = new Point(0,0);
+		
+		//Test 1, check case where area is smaller than AREA1.
+		Point[] data1 = {c, filler, b ,filler, a, filler, d, filler, e}; //cba = 0.5, ade = 1.
+		CMV cmv1 = new CMV(9, data1, param);
+		//Should be true, since cba < AREA2 and ade > AREA1.
+		assertThat(cmv1.DECIDE()[14], equalTo(true));
+		
+		//Test 2, check case where only one condition is met.
+		param.AREA1 = 2;
+		param.AREA2 = 2;
+		CMV cmv2 = new CMV(9, data1, param);
+		//Should be false, since cba and ade < AREA2 but biggest triangle ade (=1) not bigger AREA1 or AREA2.
+		assertThat(cmv2.DECIDE()[14], equalTo(false));
+		
+		//Test 3, too little data;
+		Point[] data2 = {a, b, c, d};
+		CMV cmv3 = new CMV(4, data2, param);
+		//Should be false, only 4 data points, need 5.
+		assertThat(cmv3.DECIDE()[14], equalTo(false));
+	}
 
 }
